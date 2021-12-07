43de3379e729feeaf25a9ca5b7d354dc91ed694b
Holly <milstowers@gmail.com>
2021-12-07 21:56:20 +0000

    Updates

---
+++ precache-manifest.89e79621887fdab580c07070a1df94a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54c06d5ddc50111f712a7808a032596c",
+    "url": "/bank/index.html"
+  },
+  {
+    "revision": "ce1ff7c4a5f67456f7df",
+    "url": "/bank/static/css/main.1ef17912.chunk.css"
+  },
+  {
+    "revision": "d871006bca97a6cdc468",
+    "url": "/bank/static/js/2.3fae16bf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/bank/static/js/2.3fae16bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce1ff7c4a5f67456f7df",
+    "url": "/bank/static/js/main.0cd71a9b.chunk.js"
+  },
+  {
+    "revision": "a04bbd797a74b672027d",
+    "url": "/bank/static/js/runtime-main.4aa3dbe4.js"
+  }
+]);
